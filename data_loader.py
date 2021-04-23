f354480393e82cc5f5c3920eed41084fdb2f8d0c
Amanda <amb467@pitt.edu>
2021-04-23 19:02:43 -0400

    m

--- data_loader.py
+++ data_loader.py
@@ -14,7 +14,7 @@ class CocoDataset(data.Dataset):
         self.vocab = vocab
         self.transform = transform
         vk = np.asarray(self.vocab.values())
-        print(f'Vocab: {vk}')
+        #print(f'Vocab: {vk}')
         self.vocab_mean = np.mean(np.asarray(self.vocab.values()), axis=0)
         print(f'Mean size: {self.vocab_mean.size()}')
 
